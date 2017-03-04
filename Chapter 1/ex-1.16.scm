d5149a17e9846592dc733c53338148dab48253cd
haerfest <wouter.hobers+github@gmail.com>
2017-03-04 20:31:55 +0100

    Work through book.

--- Chapter 1/ex-1.16.scm
+++ Chapter 1/ex-1.16.scm
@@ -1,12 +1,37 @@
+
+; When n even:
+; (iter a b n)
+; => {definition}
+; a * b^n
+; => {n even, arithmetic}
+; a * b^(2n/2)
+; => {arithmetic}
+; a * (b^2)^(n/2)
+; => {definition}
+; (iter a (* b b) (/ n 2))
+;
+; When n odd:
+; (iter a b n)
+; => {definition}
+; a * b^n
+; => {n odd, arithmetic}
+; a * b^(1+n-1)
+; => {arithmetic}
+; a * b^1 * b^(n-1)
+; => {arithmetic}
+; (a * b) * b^(n-1)
+; => {definition}
+; (iter (* a b) b (- n 1))
+
 (define (fast-expt b n)
   (define (even? x)
     (= (remainder x 2) 0))
-  (define (fast-expt-iter a b n)
+  (define (iter a b n)
     (cond ((= n 0) a)
-          ((even? n)  (fast-expt-iter a          ; a * b^n = a * (b^2)^(n/2)
-                                      (* b b)
-                                      (/ n 2)))
-          (else (fast-expt-iter (* a b)          ; a * b^n = (a * b) * b^(n-1)
-                                b
-                                (- n 1)))))
-  (fast-expt-iter 1 b n))
+          ((even? n)  (iter a
+                            (* b b)
+                            (/ n 2)))
+          (else (iter (* a b)
+                      b
+                      (- n 1)))))
+  (iter 1 b n))
