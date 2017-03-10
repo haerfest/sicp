296b7a3bdffc76dddf90e368f2fe23101b44bce5
haerfest <wouter.hobers+github@gmail.com>
2017-03-10 23:43:28 +0100

    Work through book.

--- Chapter 1/ex-1.26.scm
+++ Chapter 1/ex-1.26.scm
@@ -2,20 +2,20 @@
 ; prior to calling the function.  This means that in (square x), the argument x
 ; is evaluated once, and in (* x x), the argument x is evaluated twice.
 ;
-; Here the (expmod ...) expression is evaluated once, and the reduction of the
-; problem size by (/ exp 2) results in an order of growth O(log(n)):
+; Below the (expmod ...) expression is evaluated once, and the reduction of the
+; problem size by (/ exp 2) results in an order of growth O(log2(n)):
 ;
 ;   (square (expmod base (/ exp 2) m))
 ;
-; But here both (expmod ...) expressions are evaluated, and even though each
+; But below both (expmod ...) expressions are evaluated, and even though each
 ; evaluation is on half the problem size, because we are doing it twice for
-; every step, it is effectively O(2^n) * O(log2(n)), cancelling each other
+; every step, it is effectively O(2^n * log2(n)), cancelling each other
 ; out:
 ; 
 ;   (* (expmod base (/ exp 2) m)
 ;      (expmod base (/ exp 2) m))
 ;
-; The net order of growth is therefore O(n).
+; The net order of growth is therefore just O(n).
 
 (define (expmod base exp m)
   (cond ((= exp 0) 1)
