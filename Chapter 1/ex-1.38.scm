60c96cf196844805fd6a0640957b76862d570717
haerfest <wouter.hobers+github@gmail.com>
2017-03-26 00:23:17 +0100

    Work through book.

--- Chapter 1/ex-1.38.scm
+++ Chapter 1/ex-1.38.scm
@@ -10,12 +10,13 @@
   (floor (/ a b)))
 
 (define (e k)
-  (cont-frac (lambda (i) 1.0)
-             (lambda (i)
-               (let ((j (remainder (- i 1) 3)))
-                 (cond ((= j 0) 1.0)
-                       ((= j 1) (+ 2.0 (* 2.0 (div (- i 1) 3))))
-                       ((= j 2) 1.0))))
-             k))
+  (+ 2.0
+     (cont-frac (lambda (i) 1.0)
+                (lambda (i)
+                  (let ((j (remainder (- i 1) 3)))
+                    (cond ((= j 0) 1.0)
+                          ((= j 1) (+ 2.0 (* 2.0 (div (- i 1) 3))))
+                          ((= j 2) 1.0))))
+                k)))
 
-(e 20)  ; => 0.7182818284590452
+(e 20)  ; => 2.7182818284590452
