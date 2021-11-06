ae21d662a12aff100c7b4a5d6e6c0cad1c3ef7d2
Wouter Hobers <who@MacBookPro.local>
2021-11-07 00:57:55 +0100

    Work through book.

---
+++ Chapter 2/ex-2.36.scm
@@ -0,0 +1,9 @@
+(load "ex-2.33.scm")                    ; for accumulate
+
+(define nil '())
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
