9f982d9c329d9ebf63cbb0a14ae02b7acbac6eff
Wouter Hobers <who@MacBookPro.local>
2021-11-08 15:50:36 +0100

    Work through book.

---
+++ Chapter 2/ex-2.37.scm
@@ -0,0 +1,38 @@
+;; To use the map-on-steroids:
+;;
+;; $ racket
+;; > (require sicp)
+;; > (display (map + '(1 2 3) '(4 5 6)))
+;; (5 7 9)
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+(define (matrix-*-vector m v)
+  (map (lambda (row)
+         (dot-product row v))
+       m))
+
+(define (transpose mat)
+  (accumulate-n (lambda (row acc)
+                  (cons row acc))
+                nil
+                mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (row)
+           (matrix-*-vector cols row))
+         m)))
