24f808377406f26128b5afaf1c41a2c5b344f281
Wouter Hobers <who@MacBookPro.local>
2021-11-10 01:00:28 +0100

    Work through book.

--- Chapter 2/ex-2.40.scm
+++ Chapter 2/ex-2.40.scm
@@ -1,7 +1,16 @@
+(load "../Chapter 1/ex-1.33.scm")       ; prime?
 (load "ex-2.33.scm")                    ; accumulate
 
 (define (flatmap proc seq)
   (accumulate append nil (map proc seq)))
+
+(define (filter pred items)
+  (define (iter items result)
+    (cond ((null? items) result)
+          ((pred (car items)) (iter (cdr items)
+                                    (cons (car items) result)))
+          (else (iter (cdr items) result))))
+  (iter items '()))
 
 (define (enumerate-interval i j)
   (if (> i j)
@@ -13,3 +22,16 @@
              (map (lambda (j) (list j i))
                   (enumerate-interval 1 (- i 1))))
            (enumerate-interval 1 n)))
+
+(define (make-pair-sum pair)
+  (let ((j (car pair))
+        (i (cadr pair)))
+    (list j i (+ j i))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum?
+               (unique-pairs n))))
